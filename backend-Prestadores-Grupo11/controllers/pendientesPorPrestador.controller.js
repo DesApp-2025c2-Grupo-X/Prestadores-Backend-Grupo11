454efc75993fc1541ee0351091c0e27c300a2f15
juancamargo02 <juaninachocamargo@hotmail.com>
2025-11-05 20:25:57 -0300

    Cambio pendientesPorPrestador para que me traiga la informacion del integrante

--- backend-Prestadores-Grupo11/controllers/pendientesPorPrestador.controller.js
+++ backend-Prestadores-Grupo11/controllers/pendientesPorPrestador.controller.js
@@ -1,16 +1,43 @@
-const { Autorizacion, Receta, Reintegro } = require('../db/models');
+const { Autorizacion, Receta, Reintegro, Integrante } = require('../db/models');
 
 const getPendientesPorPrestador = async (req, res) => {
   const prestadorId = req.params.id; 
   console.log('Entró al endpoint con ID:', req.params.id);
 
   const [autorizaciones, recetas, reintegros] = await Promise.all([
-    Autorizacion.findAll({ where: { estado: 'en analisis', usuarioUltimoCambio: prestadorId } }),
+      Autorizacion.findAll({
+        where: { estado: 'en analisis', usuarioUltimoCambio: prestadorId },
+        include: [
+          {
+            model: Integrante,
+            as: 'integrante',
+            attributes: ['id', 'nombre', 'dni', 'edad', 'afiliadoId']
+          }
+        ]
+      }),
 
-    Receta.findAll({ where: { estado: 'en analisis', usuarioUltimoCambio: prestadorId } }),
+      Receta.findAll({
+        where: { estado: 'en analisis', usuarioUltimoCambio: prestadorId },
+        include: [
+          {
+            model: Integrante,
+            as: 'integrante',
+            attributes: ['id', 'nombre', 'dni', 'edad', 'afiliadoId']
+          }
+        ]
+      }),
 
-    Reintegro.findAll({ where: { estado: 'en analisis', usuarioUltimoCambio: prestadorId } })
-  ]);
+      Reintegro.findAll({
+        where: { estado: 'en analisis', usuarioUltimoCambio: prestadorId },
+        include: [
+          {
+            model: Integrante,
+            as: 'integrante',
+            attributes: ['id', 'nombre', 'dni', 'edad', 'afiliadoId']
+          }
+        ]
+      }),
+    ]);
 
   res.status(200).json({ autorizaciones, recetas, reintegros });
 
