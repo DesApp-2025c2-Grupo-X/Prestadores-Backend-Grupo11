5cffff94d3b81b02d2495d7b125ee21bdab8422c
ERIKA <erikarocio.camposvera@estudiantes.unahur.edu.ar>
2025-11-12 12:10:32 -0300

    refactor turnos controller

--- backend-Prestadores-Grupo11/controllers/turnos.controller.js
+++ backend-Prestadores-Grupo11/controllers/turnos.controller.js
@@ -1,51 +1,105 @@
-const {Turno, Afiliado, Integrante, Prestador} = require('../db/models');
+const { Turno, Afiliado, Integrante, Prestador } = require("../db/models");
 
-const getAllTurnosByPrestadorId = async (req,res) => {
-    const id = req.params.prestadorId;
-    const turnos = await Turno.findAll({where: {prestadorId: id},
+const getAllTurnosByPrestadorId = async (req, res) => {
+  const id = req.params.prestadorId;
+  const turnos = await Turno.findAll({
+    where: { prestadorId: id },
+   include: [
+  {
+    model: Prestador,
+    attributes: { exclude: ["especialidad", "password"] },
+    as: "prestador",
+  },
+  {
+    model: Afiliado,
+    as: "afiliado",
     include: [
-        {model: Prestador, attributes: {exclude: ['especialidad', 'password']}, as: 'prestador'},
-        {model: Afiliado, as: 'afiliado', include: [{model: Integrante, as: 'integrantes'}]}
-    ]});
-    res.status(200).json(turnos);
-}
+      { model: Integrante, attributes: ["id", "nombre", "edad", "dni"], as: "integrantes" }
+    ],
+  },
+  {
+    model: Integrante,
+    as: "integrante",  
+    attributes: ["id", "nombre", "edad", "dni"]
+  }
+]
+  });
+  res.status(200).json(turnos);
+};
 
-const getAllTurnos = async (req,res) => {
-    const turnos = await Turno.findAll({
+const getAllTurnos = async (req, res) => {
+  const turnos = await Turno.findAll({
     include: [
-        {model: Prestador, attributes: {exclude: ['password']}, as: 'prestador'},
-        {model: Afiliado, as: 'afiliado', include: [{model: Integrante, as: 'integrantes'}]},
-    ]});
-    res.status(200).json(turnos);
-}
+      {
+        model: Prestador,
+        attributes: { exclude: ["password"] },
+        as: "prestador",
+      },
+      {
+        model: Afiliado,
+        as: "afiliado",
+        include: [{ model: Integrante, as: "integrantes" }],
+      },
+      { model: Integrante, as: "integrante" },// para el turno asosiado a un integrante
+    ],
+  });
+  res.status(200).json(turnos);
+};
 
-const getAllTurnosByEspecialidad = async (req,res) => {
-    const e = req.params.especialidad;
-    const turnos = await Turno.findAll({
+const getAllTurnosByEspecialidad = async (req, res) => {
+  const e = req.params.especialidad;
+  const turnos = await Turno.findAll({
     include: [
-        {model: Prestador, attributes: {exclude: ['password']}, as: 'prestador', where: {especialidad: e}},
-        {model: Afiliado, as: 'afiliado', include: [{model: Integrante, as: 'integrantes'}]}
-    ]});
-    res.status(200).json(turnos);
-}
+      {
+        model: Prestador,
+        attributes: { exclude: ["password"] },
+        as: "prestador",
+        where: { especialidad: e },
+      },
+      {
+        model: Afiliado,
+        as: "afiliado",
+        include: [{ model: Integrante, as: "integrantes" }],
+      },
+    ],
+  });
+  res.status(200).json(turnos);
+};
 
-const getAllTurnosByMedico = async (req,res) => {
-    const m = req.params.medico;
-    const turnos = await Turno.findAll({
+const getAllTurnosByMedico = async (req, res) => {
+  const m = req.params.medico;
+  const turnos = await Turno.findAll({
     include: [
-        {model: Prestador, attributes: {exclude: ['password']}, as: 'prestador', where: {username: m, role: "medico"}},
-        {model: Afiliado, as: 'afiliado', include: [{model: Integrante, as: 'integrantes'}]}
-    ]});
-    res.status(200).json(turnos);
-}
+      {
+        model: Prestador,
+        attributes: { exclude: ["password"] },
+        as: "prestador",
+        where: { username: m, role: "medico" },
+      },
+      {
+        model: Afiliado,
+        as: "afiliado",
+        include: [{ model: Integrante, as: "integrantes" }],
+      },
+    ],
+  });
+  res.status(200).json(turnos);
+};
 
-const updateNotesById = async (req,res) => {
-    const id = req.params.id;
-    const {notes} = req.body;
-    const turno = await Turno.findByPk(id);
-    turno.notes = notes;
-    await turno.save()
-    res.status(200).json(turno);
-}
+const updateNotesById = async (req, res) => {
+  const id = req.params.id;
+  const { notes } = req.body;
+  const turno = await Turno.findByPk(id);
+  turno.notes = notes;
+  await turno.save();
+  res.status(200).json(turno);
+};
 
-module.exports = {getAllTurnosByPrestadorId, updateNotesById, getAllTurnos, getAllTurnosByEspecialidad, getAllTurnos, getAllTurnosByMedico};
+module.exports = {
+  getAllTurnosByPrestadorId,
+  updateNotesById,
+  getAllTurnos,
+  getAllTurnosByEspecialidad,
+  getAllTurnos,
+  getAllTurnosByMedico,
+};
