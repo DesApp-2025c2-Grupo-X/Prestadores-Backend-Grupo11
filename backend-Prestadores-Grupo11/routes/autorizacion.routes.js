70d599f7eff9b29ef1aa82c0c9fc490b71a2cd8e
juancamargo02 <juaninachocamargo@hotmail.com>
2025-11-26 15:12:43 -0300

    Agrego los controllers para poder solucionar lo de las solicitudes

--- backend-Prestadores-Grupo11/routes/autorizacion.routes.js
+++ backend-Prestadores-Grupo11/routes/autorizacion.routes.js
@@ -4,6 +4,7 @@ const controller = require('../controllers/autorizacion.controller');
 const validarId = require('../middlewares/validarId.middleware');
 const validarCambioEstado = require('../middlewares/validarCambioEstado');
 const existeAutorizacion = require('../middlewares/existeAutorizacion.middleware');
+const { genericMiddleware } = require('../middlewares');
 const { Autorizacion } = require('../db/models');
 
 router.get('/', controller.listar);
@@ -15,6 +16,10 @@ router.get('/bandeja', controller.listarPorEstado);
 router.get('/dashboard', controller.dashboard);
 
 router.get('/:id', existeAutorizacion, controller.obtenerPorId);
+
+router.get('/pendientes/prestadorId/:prestadorId', genericMiddleware.validateRolById("medico"), controller.getPendientesPrestador)
+
+router.get('/pendientes/centroId/:prestadorId', genericMiddleware.validateRolById("centro_medico"), controller.getPendientesCentro)
 
 
 module.exports = router;
--- backend-Prestadores-Grupo11/routes/reintegro.routes.js
+++ backend-Prestadores-Grupo11/routes/reintegro.routes.js
@@ -5,21 +5,23 @@ const reintegroController = require('../controllers/reintegro.controller');
 const validarId = require('../middlewares/validarId.middleware');
 const existeReintegro = require('../middlewares/existeReintegro.middleware');
 const validarCambioEstado = require('../middlewares/validarCambioEstado');
+const { genericMiddleware } = require('../middlewares');
 
 const db = require('../db/models');
 const { Reintegro } = db;
 
 router.get('/', reintegroController.listar);
 
-
 router.get('/estado', reintegroController.listarPorEstado);
 
-
 router.get('/dashboard', reintegroController.dashboard);
-
 
 router.get('/:id', validarId, existeReintegro, reintegroController.obtenerPorId);
 
 router.put('/:id/estado', validarId, validarCambioEstado(Reintegro, 'reintegro'), reintegroController.cambiarEstado);
+
+router.get('/pendientes/prestadorId/:prestadorId', genericMiddleware.validateRolById("medico"), reintegroController.getPendientesPrestador)
+
+router.get('/pendientes/centroId/:prestadorId', genericMiddleware.validateRolById("centro_medico"), reintegroController.getPendientesCentro)
 
 module.exports = router;
