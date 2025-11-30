64e49801b3288612067daf199c1a5353dd213f8f
ERIKA <erikarocio.camposvera@estudiantes.unahur.edu.ar>
2025-11-30 14:48:22 -0300

    refactor dashboardcontroller

--- backend-Prestadores-Grupo11/routes/dashboard.route.js
+++ backend-Prestadores-Grupo11/routes/dashboard.route.js
@@ -1,10 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {genericController} = require('../controllers');
-const {Prestador} = require('../db/models');
 
-router.get('/:id',
-  genericController.getModelById(Prestador)
-);
+const {
+  getKpis,
+  getSemanal,
+  getMensual,
+  getAnual,
+  getRegistros,
+  getFiltrado,
+} = require("../controllers/dashboard.controller");
+
+// Rutas del dashboard
+router.get("/kpis", getKpis);
+router.get("/semanal", getSemanal);
+router.get("/mensual", getMensual);
+router.get("/anual", getAnual);
+router.get("/registros", getRegistros);
+router.get("/filtrado", getFiltrado);
 
 module.exports = router;
